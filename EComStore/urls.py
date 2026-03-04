a66df6032fe3e6846b8c480433a6a25fba0366a4
Vishal Hegde <hegdevishals75@gmail.com>
2026-03-04 21:02:26 +0530

    Clean up and format URL configuration comments

--- EComStore/urls.py
+++ EComStore/urls.py
@@ -19,11 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
